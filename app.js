5568a5e2309081ad30e52ab5a6dee7c41ff679ae
pawelkudlak <coodypl@gmail.com>
2022-10-11 20:30:06 +0200

    X Y roundCount

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let xOrY = "X";
+
 const start = () => {
     if (document.getElementById("menuButton")) {
         refresh()
@@ -8,15 +10,7 @@ const start = () => {
 }
 
 const menu = () => {
-    // const menu = document.createElement("div");
-    // menu.setAttribute("id", "menuButton");
-    // const textMenu = document.createTextNode("Hello in New Game");
-
-    // menu.appendChild(textMenu);
-
-    // document.getElementById("mainDiv").appendChild(menu);
     createLevel();
-    // roundXO();
 }
 
 const refresh = () => {
@@ -25,33 +19,34 @@ const refresh = () => {
 }
 
 const createLevel = () => {
+
     const d1 = document.createElement("div");
     d1.setAttribute("id", "d1");
-    // d1.addEventListener('click', click);
+
     const d2 = document.createElement("div");
     d2.setAttribute("id", "d2");
-    // d2.addEventListener('click', click);
+
     const d3 = document.createElement("div");
     d3.setAttribute("id", "d3");
-    // d3.addEventListener('click', click);
+
     const d4 = document.createElement("div");
     d4.setAttribute("id", "d4");
-    // d4.addEventListener('click', click);
+
     const d5 = document.createElement("div");
     d5.setAttribute("id", "d5");
-    // d5.addEventListener('click', click);
+
     const d6 = document.createElement("div");
     d6.setAttribute("id", "d6");
-    // d6.addEventListener('click', click);
+
     const d7 = document.createElement("div");
     d7.setAttribute("id", "d7");
-    // d7.addEventListener('click', click);
+
     const d8 = document.createElement("div");
     d8.setAttribute("id", "d8");
-    // d8.addEventListener('click', click);
+
     const d9 = document.createElement("div");
     d9.setAttribute("id", "d9");
-    // d9.addEventListener('click', click);
+
 
     document.getElementById("mainDiv").appendChild(d1);
     document.getElementById("mainDiv").appendChild(d2);
@@ -70,17 +65,13 @@ const createLevel = () => {
 
 const roundXO = () => {
     const witchPlayer = document.getElementById("round");
-    const roundCount = 1;
-    const player = "X";
 
-    for (roundCount; roundCount <= 8; roundCount++) {
-        if (player === "X") {
-            witchPlayer.innerText = `Runda: ${roundCount}\nRuch gracza: ${player}`;
-            player = "Y";
+    for (const roundCount = 1; roundCount <= 9; roundCount++) {
+        if (xOrY === "X") {
+            witchPlayer.innerText = `Runda: ${roundCount}\nRuch gracza: ${xOrY}`;
         }
         else {
-            witchPlayer.innerText = `Runda: ${roundCount}\nRuch gracza: ${player}`;
-            player = "X";
+            witchPlayer.innerText = `Runda: ${roundCount}\nRuch gracza: ${xOrY}`;
         }
     }
 }
@@ -98,9 +89,25 @@ const click = () => {
 
     const divArray = [d1, d2, d3, d4, d5, d6, d7, d8, d9];
 
-    divArray.forEach(div => {
-        div.addEventListener('click', (useBlock) => {
-            document.getElementById(div.id).style.cursor="no-drop";
+    const witchPlayer = document.getElementById("round");
+    let roundCount = 1;
+    witchPlayer.innerText = `Runda: ${roundCount}\nRuch gracza: ${xOrY}`;
+
+        divArray.forEach(div => {
+            div.addEventListener('click', (useBlock) => {
+                roundCount++;
+                if (div.innerHTML === "") {
+                    document.getElementById(div.id).style.cursor = "no-drop";
+                    
+                    if (xOrY == "X") {
+                        document.getElementById(div.id).innerText = xOrY;
+                        xOrY = "Y";
+                    } else {
+                        document.getElementById(div.id).innerText = xOrY;
+                        xOrY = "X";
+                    }
+                }
+                witchPlayer.innerText = `Runda: ${roundCount}\nRuch gracza: ${xOrY}`;
+            })
         })
-    })
 }
